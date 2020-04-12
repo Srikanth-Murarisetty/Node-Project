3041f08f2b98921afc6205623c867b89c0ce59af
Srikanth-Murarisetty <49820803+Srikanth-Murarisetty@users.noreply.github.com>
2020-04-12 18:36:16 +0400

    Initial Commit
    With Ganesh blessing, started a new file.

---
+++ main.js
@@ -0,0 +1,16 @@
+// There are  2parts to a NODE file 1. Import ( require http) 2. CreateServer
+
+var http = require("http");
+
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
